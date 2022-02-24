47e6e10a9c3746ce7a0dbc1ac5757c9426a5015c
Andre <andre360.ars@gmail.com>
2022-02-24 19:27:30 -0300

    Requisito 9 - Adiciona botao, que exclui a carta correspondente

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
     this.attrValidation = this.attrValidation.bind(this);
     this.saveValidation = this.saveValidation.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.removeCard = this.removeCard.bind(this);
   }
 
   onInputChange({ target }) {
@@ -102,6 +103,13 @@ class App extends React.Component {
     });
   }
 
+  removeCard(name, trunfo) {
+    const { data } = this.state;
+
+    if (trunfo) this.setState({ hasTrunfo: false });
+    this.setState(({ data: data.filter((info) => info.cardName !== name) }));
+  }
+
   render() {
     const { state: {
       cardName,
@@ -115,37 +123,51 @@ class App extends React.Component {
       isSaveButtonDisabled,
       hasTrunfo,
       data,
-    }, onInputChange, onSaveButtonClick } = this;
+    }, onInputChange, onSaveButtonClick, removeCard } = this;
 
     return (
-      <main className="main-container">
-        <Form
-          cardName={ cardName }
-          onInputChange={ onInputChange }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
-          hasTrunfo={ hasTrunfo }
-          isSaveButtonDisabled={ isSaveButtonDisabled }
-          onSaveButtonClick={ onSaveButtonClick }
-        />
-        <Card
-          cardName={ cardName }
-          cardDescription={ cardDescription }
-          cardAttr1={ cardAttr1 }
-          cardAttr2={ cardAttr2 }
-          cardAttr3={ cardAttr3 }
-          cardImage={ cardImage }
-          cardRare={ cardRare }
-          cardTrunfo={ cardTrunfo }
-        />
+      <main>
+        <section className="main-container">
+          <Form
+            cardName={ cardName }
+            onInputChange={ onInputChange }
+            cardDescription={ cardDescription }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardTrunfo={ cardTrunfo }
+            hasTrunfo={ hasTrunfo }
+            isSaveButtonDisabled={ isSaveButtonDisabled }
+            onSaveButtonClick={ onSaveButtonClick }
+          />
+          <Card
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardAttr1={ cardAttr1 }
+            cardAttr2={ cardAttr2 }
+            cardAttr3={ cardAttr3 }
+            cardImage={ cardImage }
+            cardRare={ cardRare }
+            cardTrunfo={ cardTrunfo }
+          />
+        </section>
         {
-          data.map((info) => <Card { ...info } key={ cardName } />)
+          data.map((info) => (
+            <div className="card" key={ info.cardName }>
+              <Card { ...info } key={ info.cardName } />
+              <button
+                type="button"
+                data-testid="delete-button"
+                key={ info.cardName }
+                onClick={ () => removeCard(info.cardName, info.cardTrunfo) }
+              >
+                Excluir
+              </button>
+            </div>))
         }
+
       </main>
     );
   }
